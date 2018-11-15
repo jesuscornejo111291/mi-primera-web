923685749fb188e2820a86f3ca80d7a3141979e2
JesusCornejo <jesus.cornejo@vallegrande.edu.pe>
2018-11-15 12:04:01 -0500

    he agregado mas ejercicios en hola mundo

--- lab01/javascritp/holamundo.js
+++ lab01/javascritp/holamundo.js
@@ -7,6 +7,14 @@ console.log("mala");
 console.log("san vicente");
 console.log(100+500);
 console.log(800<500);
+console.log("peru,brazil,volibia");
+const.console.error("este error es de prueba");
+console.console.warn("esto es una prueba");
+ console.info("multiplicar tres numeros")
+ console.log(25*10);
+ 
+
+
 
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 #mis primeros pasos con javascript
 
 empesaremos a conocer el lenguaje de javascript
+
+##laboratorio 01
+en este laboratorio hemos hechos cambios
