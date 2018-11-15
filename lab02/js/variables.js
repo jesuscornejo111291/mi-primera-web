21ea1fb4d582766cc960341a810c7d2b7311b3cd
JesusCornejo <jesus.cornejo@vallegrande.edu.pe>
2018-11-15 13:27:59 -0500

    hemos trabajado con canales

---
+++ lab02/js/variables.js
@@ -0,0 +1,33 @@
+var nombres="fernando";
+console.log(nombres);
+var nompersona="gloria";
+var apepersonas="guerra paz";
+var edadpersona= 20;
+var provpersona="san vicente";
+console.log(nompersona, apepersonas);
+console.log(edadpersona);
+var num1=100;
+var num2=250;
+var suma=num1+num2;
+var resta=num1-num2;num1-num2;
+var divicion=num1/num2;
+var multiplicacion=num1*num2
+var igual=num1>num2;
+var comparar=num1==num2;
+console.log("la suma del " + num1 + " mas " + num2 + " es " + suma);
+console.log("la resta del " + num1 + " menos " + num2 + " es " + resta);
+console.log(" la divisionde " + num1 + " entre " + num2 + " es " + divicion);
+console.log("la multiplicacion del " + num1 + " por " + num2 + " es " + multiplicacion);
+var producto="zapatillas";
+var precio= 380;
+var cantidad=10;
+var total= precio*cantidad;
+console.log(" el predio " + precio +" por " + cantidad + " es " + total);
+
+
+
+
+
+
+
+
